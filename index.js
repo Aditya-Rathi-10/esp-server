541d189f7b19943ceba3ee91f90db3e4133476ea
AdityaRathi-10 <adityarathi3916@gmail.com>
2026-05-04 01:29:30 +0530

    add / route

--- index.js
+++ index.js
@@ -5,10 +5,14 @@ const PORT = 3000
 
 app.use(express.json())
 
+app.get("/", (req, res) => {
+    return res.json({ message: "hello" })
+})
+
 app.post("/api/data", (req, res) => {
     const data = req.body
     console.log("data", data)
-    return res.json({ message: "hello" })
+    return res.json({ message: "data aa gaya" })
 })
 
 app.listen(PORT, () => console.log("Server started"))
